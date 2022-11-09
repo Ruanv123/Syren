9c7e74c0dac676dc577dffaea67fc1a951061130
Ruan Victor <ruanvictormr@gmail.com>
2022-11-09 10:19:03 -0300

    excluindo o carrinho dropdwon q esta muito bugado

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,9 +11,3 @@ $(document).ready(function () {
         $(".shopping-cart").fadeToggle("fast");
     });
 })();
-
-var contagem = document.getElementsByClassName('clearfix').length;
-
-var valor = document.querySelectorAll('.badge');
-
-contagem.textContent = valor;
