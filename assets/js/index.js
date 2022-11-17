5aeba1395d0dfd8f4717082d07f9c047ac387348
Ruan Victor <ruanvictormr@gmail.com>
2022-11-17 20:52:34 -0300

    carrousel teste

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,64 @@
-$(document).ready(function () {
-    $(".hamburguer").click(function () {
-        $(this).toggleClass("hamburguer-active");
-        $(".nav-list-mobile").toggleClass("active");
-    });
-});
+/*======== Menu Responsivo ========*/
+const searchButton = document.querySelector("nav .desktop-nav .link-search"); 
+const closeButton = document.querySelector(".search-container .link-close");
+const desktopNav = document.querySelector(".desktop-nav");
+const searchContainer = document.querySelector(".search-container");
+const overlay = document.querySelector(".overlay");
 
-/* carrinho dropdown */
-(function () {
-    $(".cart").on("click", function () {
-        $(".shopping-cart").fadeToggle("fast");
-    });
-})();
+searchButton.addEventListener("click", () => {
+    desktopNav.classList.add("hide");
+    searchContainer.classList.remove("hide");
+    overlay.classList.add("show");
+})
+
+closeButton.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+overlay.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+
+// Mobile Version
+
+const menuIconContainer = document.querySelector("nav .menu-icon-container");
+const navContainer = document.querySelector(".nav-container");
+
+menuIconContainer.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+})
+
+
+const searchBar = document.querySelector(".mobile-search-container .search-bar");
+const nav = document.querySelector(".nav-container nav");
+const searchInput = document.querySelector(".mobile-search-container input");
+const cancelBtn = document.querySelector(".mobile-search-container .cancel-btn");
+
+searchInput.addEventListener("click", () => {
+    searchBar.classList.add("active");
+    nav.classList.add("move-up");
+    desktopNav.classList.add("move-down");
+})
+
+cancelBtn.addEventListener("click", () => {
+    searchBar.classList.remove("active");
+    nav.classList.remove("move-up");
+    desktopNav.classList.remove("move-down");
+})
+
+/*======== SWIPER ========*/
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 50,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
